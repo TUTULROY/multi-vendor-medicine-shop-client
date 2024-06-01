9cd03f22da66d60277f67a6d8b58dc84cd4cd82d
TUTULROY <tutulroyjoy@gmail.com>
2024-06-01 15:58:24 +0600

    Start My Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-3xl text-center'>vite + react</h1>
+     
+    </>
+  )
+}
+
+export default App
