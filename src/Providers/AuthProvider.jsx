ba7b01cfe3256ba0f56eb7ddbee788ce9856863d
TUTULROY <tutulroyjoy@gmail.com>
2024-06-02 23:30:32 +0600

    added some work with provider

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,44 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/firebase.config";
 
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-const AuthProvider = () => {
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const updateUserProfile = (name, photo) =>{
+       return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+    useEffect(()=>{
+      const unsubscribe =  onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            console.log('current user', currentUser);
+            setLoading(false);
+        });
+        return () =>{
+            return unsubscribe();
+        }
+    },[])
+
+    const authInfo ={
+        user,
+        createUser,
+        updateUserProfile,
+        loading
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
